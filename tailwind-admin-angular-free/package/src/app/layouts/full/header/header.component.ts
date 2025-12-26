8909fd4db97f74764f5003151096c767287b1923
wrappixelTeam <wrappixelteam.2016@gmail.com>
2025-12-26 17:44:41 +0530

    ''

--- tailwind-admin-angular-free/package/src/app/layouts/full/header/header.component.ts
+++ tailwind-admin-angular-free/package/src/app/layouts/full/header/header.component.ts
@@ -32,30 +32,16 @@ interface profiledd {
   link: string;
 }
 
-interface apps {
-  id: number;
-  img: string;
-  title: string;
-  subtitle: string;
-  link: string;
-}
-
-interface quicklinks {
-  id: number;
-  title: string;
-  link: string;
-}
-
 @Component({
-    selector: 'app-header',
-    imports: [
+  selector: 'app-header',
+  imports: [
     RouterModule,
     NgScrollbarModule,
     TablerIconsModule,
     MaterialModule
-],
-    templateUrl: './header.component.html',
-    encapsulation: ViewEncapsulation.None
+  ],
+  templateUrl: './header.component.html',
+  encapsulation: ViewEncapsulation.None
 })
 export class HeaderComponent {
   @Input() showToggle = true;
@@ -65,30 +51,6 @@ export class HeaderComponent {
   @Output() toggleCollapsed = new EventEmitter<void>();
 
   showFiller = false;
-
-  public languages: any[] = [
-    {
-      language: 'English',
-      code: 'en',
-      type: 'US',
-      icon: '/assets/images/flag/icon-flag-en.svg',
-    },
-    {
-      language: 'Español',
-      code: 'es',
-      icon: '/assets/images/flag/icon-flag-es.svg',
-    },
-    {
-      language: 'Français',
-      code: 'fr',
-      icon: '/assets/images/flag/icon-flag-fr.svg',
-    },
-    {
-      language: 'German',
-      code: 'de',
-      icon: '/assets/images/flag/icon-flag-de.svg',
-    },
-  ];
 
   @Output() optionsChange = new EventEmitter<AppSettings>();
 
@@ -165,108 +127,6 @@ export class HeaderComponent {
       img: '/assets/images/svgs/icon-tasks.svg',
       title: 'My Tasks',
       subtitle: 'To-do and Daily Tasks',
-      link: '/',
-    },
-  ];
-
-  apps: apps[] = [
-    {
-      id: 1,
-      img: '/assets/images/svgs/icon-dd-chat.svg',
-      title: 'Chat Application',
-      subtitle: 'Messages & Emails',
-      link: '/',
-    },
-    {
-      id: 2,
-      img: '/assets/images/svgs/icon-dd-cart.svg',
-      title: 'eCommerce App',
-      subtitle: 'Buy a Product',
-      link: '/',
-    },
-    {
-      id: 3,
-      img: '/assets/images/svgs/icon-dd-invoice.svg',
-      title: 'Invoice App',
-      subtitle: 'Get latest invoice',
-      link: '/',
-    },
-    {
-      id: 4,
-      img: '/assets/images/svgs/icon-dd-date.svg',
-      title: 'Calendar App',
-      subtitle: 'Get Dates',
-      link: '/',
-    },
-    {
-      id: 5,
-      img: '/assets/images/svgs/icon-dd-mobile.svg',
-      title: 'Contact Application',
-      subtitle: '2 Unsaved Contacts',
-      link: '/',
-    },
-    {
-      id: 6,
-      img: '/assets/images/svgs/icon-dd-lifebuoy.svg',
-      title: 'Tickets App',
-      subtitle: 'Create new ticket',
-      link: '/',
-    },
-    {
-      id: 7,
-      img: '/assets/images/svgs/icon-dd-message-box.svg',
-      title: 'Email App',
-      subtitle: 'Get new emails',
-      link: '/',
-    },
-    {
-      id: 8,
-      img: '/assets/images/svgs/icon-dd-application.svg',
-      title: 'Courses',
-      subtitle: 'Create new course',
-      link: '/',
-    },
-  ];
-
-  quicklinks: quicklinks[] = [
-    {
-      id: 1,
-      title: 'Pricing Page',
-      link: '/t',
-    },
-    {
-      id: 2,
-      title: 'Authentication Design',
-      link: '/',
-    },
-    {
-      id: 3,
-      title: 'Register Now',
-      link: '/',
-    },
-    {
-      id: 4,
-      title: '404 Error Page',
-      link: '/',
-    },
-    {
-      id: 5,
-      title: 'Notes App',
-      link: '/',
-    },
-    {
-      id: 6,
-      title: 'Employee App',
-      link: '/',
-    },
-    {
-      id: 7,
-      title: 'Todo Application',
-      link: '/',
-    },
-    {
-      id: 8,
-      title: 'Treeview',
       link: '/',
     },
   ];
--- tailwind-admin-angular-free/package/src/app/pages/ui-components/lists/lists.component.ts
+++ tailwind-admin-angular-free/package/src/app/pages/ui-components/lists/lists.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
-import {MatIconModule} from '@angular/material/icon';
-import {DatePipe} from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { DatePipe } from '@angular/common';
 import { MaterialModule } from 'src/app/material.module';
 
 
@@ -13,36 +13,36 @@ export interface Section {
 
 @Component({
   selector: 'app-lists',
-  imports: [MatListModule, MatCardModule, DatePipe,MatIconModule, MaterialModule ],
+  imports: [MatListModule, MatCardModule, DatePipe, MatIconModule, MaterialModule],
   templateUrl: './lists.component.html',
 })
 export class AppListsComponent {
-  constructor() {}
+  constructor() { }
 
   typesOfShoes: string[] = ['Loafers', 'Sneakers'];
 
   folders: Section[] = [
     {
       name: 'Photos',
-      updated: new Date('1/1/25'),
+      updated: new Date('1/1/26'),
     },
     {
       name: 'Recipes',
-      updated: new Date('1/17/25'),
+      updated: new Date('1/17/26'),
     },
     {
       name: 'Work',
-      updated: new Date('1/28/25'),
+      updated: new Date('1/28/26'),
     },
   ];
   notes: Section[] = [
     {
       name: 'Vacation Itinerary',
-      updated: new Date('2/20/25'),
+      updated: new Date('2/20/26'),
     },
     {
       name: 'Kitchen Remodel',
-      updated: new Date('1/18/25'),
+      updated: new Date('1/18/26'),
     },
   ];
 }
